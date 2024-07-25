6d22700739e2699095ca12257ab175cd536d9d63
brine-egg <89018140+brine-egg@users.noreply.github.com>
2024-07-25 16:09:57 +1000

    I have no clue what I am doing

--- flake.nix
+++ flake.nix
@@ -2,14 +2,30 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:/nix-community/home-manager/";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    jerry = { 
+      url ="github:justchokingaround/jerry";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+  outputs = { nixpkgs, home-manager, jerry, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations."brine" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ 
+          ./home.nix
+          {
+            home.packages = [ jerry.packages.${system}.full ];
+          }
+        ];
+      };
+    };
 }
