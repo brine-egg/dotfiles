291d978616e82d1f332985e96eba096bcc6546d8
brine-egg <brineegg@gmail.com>
2024-07-25 16:20:07 +1000

    This is such a mess

--- flake.nix
+++ flake.nix
@@ -1,12 +1,15 @@
 {
-  description = "A very basic flake";
+  description = "Extremely scuffed Home Manager flake";
 
   inputs = {
+    # Nixpkgs repo
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Home Manager itself
     home-manager = {
       url = "github:/nix-community/home-manager/";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    # Jerry (anime CLI tool) repo, containing HM module
     jerry = { 
       url ="github:justchokingaround/jerry";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -15,11 +18,13 @@
 
   outputs = { nixpkgs, home-manager, jerry, ... }:
     let
+      # Basic convenience variables
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       homeConfigurations."brine" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
+        # Load HM modules
         modules = [ 
           ./home.nix
           {
