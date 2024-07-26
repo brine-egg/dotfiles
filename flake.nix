41556c39c683b495321841f31410850204472970
brine-egg <brineegg@gmail.com>
2024-07-26 15:35:44 +1000

    Why is everything scuffed

--- flake.nix
+++ flake.nix
@@ -2,35 +2,52 @@
   description = "Extremely scuffed Home Manager flake";
 
   inputs = {
-    # Nixpkgs repo
+
+    # Input basic repos
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # Home Manager itself
     home-manager = {
       url = "github:/nix-community/home-manager/";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # nixGL repo, provides workaround for OpenGL dependent packages
+    nixgl = {
+      url = "github:nix-community/nixGL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     # Jerry (anime CLI tool) repo, containing HM module
     jerry = { 
       url ="github:justchokingaround/jerry";
       inputs.nixpkgs.follows = "nixpkgs";
+
     };
   };
 
-  outputs = { nixpkgs, home-manager, jerry, ... }:
+  outputs = { nixpkgs, home-manager, jerry, nixgl, ... }:
     let
-      # Basic convenience variables
+
+      # Basic variables
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+
     in {
       homeConfigurations."brine" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
+
         # Load HM modules
         modules = [ 
           ./home.nix
-          {
-            home.packages = [ jerry.packages.${system}.full ];
+
+          { # Flake packages
+            home.packages = [ 
+              jerry.packages.${system}.full 
+            ];
           }
+
         ];
+
       };
     };
+
 }
