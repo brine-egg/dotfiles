9b8035e97bd267d9e10831d4fbb6d56b60904070
brine-egg <89018140+brine-egg@users.noreply.github.com>
2024-08-25 02:52:16 +1000

    Attempt to modularlise the nix files

---
+++ packages.nix
@@ -0,0 +1,31 @@
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  
+  # Install user level packages with Nix
+  home.packages = with pkgs; [
+    bat
+    dipc
+    fastfetch
+    fzf
+    lsd
+    nh
+    onefetch
+    ripgrep
+    ripgrep-all
+    rustup
+    spicetify-cli
+    trashy
+    tmux
+    w3m
+    yazi
+    ytfzf
+    yt-dlp
+    zoxide
+  ];
+
+}
