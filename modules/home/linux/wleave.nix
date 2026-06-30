dd7d494bd0b78ac38c19b97635a9dabdede5bff2
brine-egg <89018140+brine-egg@users.noreply.github.com>
2026-06-30 18:31:38 +1000

    refactor: Format wleave.nix

--- modules/home/linux/wleave.nix
+++ modules/home/linux/wleave.nix
@@ -6,40 +6,40 @@
   # wleave logout menu
   programs.wleave = {
     enable = true;
-	settings = {
-	  margin = 50;
-	  button-aspect-ratio = "1/1";
-	  buttons-per-row = "1/1";
-	  delay-command-ms = 50;
-	  close-on-lost-focus = true;
-	  show-keybinds = true;
-	  no-version-info = true;
-	  buttons = [
-			{
-			  label = "lock";
-			  action = "hyprlock";
-			  text = "Lock";
-			  keybind = "l";
-			}
-			{
-			  label = "logout";
-			  action = "loginctl terminate-user $USER";
-			  text = "Logout";
-			  keybind = "e";
-			}
-			{
-			  label = "shutdown";
-			  action = "systemctl poweroff";
-			  text = "Shutdown";
-			  keybind = "s";
-			}
-			{
-			  label = "reboot";
-			  action = "systemctl reboot";
-			  text = "Reboot";
-			  keybind = "r";
-			}
-		  ];
-		};
+    settings = {
+      margin = 50;
+      button-aspect-ratio = "1/1";
+      buttons-per-row = "1/1";
+      delay-command-ms = 50;
+      close-on-lost-focus = true;
+      show-keybinds = true;
+      no-version-info = true;
+      buttons = [
+        {
+          label = "lock";
+          action = "hyprlock";
+          text = "Lock";
+          keybind = "l";
+        }
+        {
+          label = "logout";
+          action = "loginctl terminate-user $USER";
+          text = "Logout";
+          keybind = "e";
+        }
+        {
+          label = "shutdown";
+          action = "systemctl poweroff";
+          text = "Shutdown";
+          keybind = "s";
+        }
+        {
+          label = "reboot";
+          action = "systemctl reboot";
+          text = "Reboot";
+          keybind = "r";
+        }
+      ];
+    };
   };
 }
