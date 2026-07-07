dbfbed920131ae7f038ba8756a1da6d30f6eb59a
brine-egg <89018140+brine-egg@users.noreply.github.com>
2026-07-07 20:16:52 +1000

    feat(home): Create Helix module

---
+++ home/config/shared/programs/helix.nix
@@ -0,0 +1,12 @@
+{
+  # Enable Helix text editor
+  programs.helix = {
+    enable = true;
+    settings = {
+      editor = {
+        color-modes = true;
+        bufferline = "multiple";
+      };
+    };
+  };
+}
