f2d1f69c1088602ad199732f1ad372ead777cd98
brine <89018140+brine-egg@users.noreply.github.com>
2026-07-22 22:49:50 +1000

    fix(nixos): add missing imports

--- nixos/modules/graphics.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs, ... }:
-{
-  fonts.fontconfig.enable = true;
-  fonts.packages = [ pkgs.nerd-fonts.roboto-mono ];
-}
---
+++ nixos/modules/system/default.nix
@@ -0,0 +1,9 @@
+{
+  imports = [
+    ./boot.nix
+	./nix.nix
+	./graphics.nix
+	./networking.nix
+	./users.nix
+  ];
+}
